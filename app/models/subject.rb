2b3cd60c67cad5b15382eb3216c6835b69080090
Taylor Huston <taylor.a.huston@gmail.com>
2015-05-10 16:03:40 -0700

    Define a one_to_one relationship between subjects and pages

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,10 +1,14 @@
 class Subject < ActiveRecord::Base
   
+  has_one :page
+  
   scope :visible, lambda { where(:visible => true) }
   scope :invisible, lambda { where(:visible => false) }
   scope :sorted, lambda {order("subjects.position ASC") }
   scope :newest_first, lambda {order("subjects.created_at ASC") }
+  
   scope :search, lambda { |query|
-    where( ["name LIKE ?", "%#{query}%"]) }
+    where( ["name LIKE ?", "%#{query}%"])
+    }
   
 end
