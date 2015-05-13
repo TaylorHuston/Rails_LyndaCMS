69d29679d77a265e82751ab2fe7bbbaab749dcdb
Taylor Huston <taylor.a.huston@gmail.com>
2015-05-12 19:08:21 -0700

    Add Index for Pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,6 @@ class Page < ActiveRecord::Base
   belongs_to :subject
   has_and_belongs_to_many :editors, :class_name => "AdminUser"
   
+  scope :newest_first, lambda {order("pages.created_at ASC") }
+  
 end
