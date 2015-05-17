41df0fa5663679b0795986d3004fac24422393a0
Taylor Huston <taylor.a.huston@gmail.com>
2015-05-16 21:30:31 -0700

    Add validation to models

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,6 +4,12 @@ class Page < ActiveRecord::Base
   has_many :sections
   has_and_belongs_to_many :editors, :class_name => "AdminUser"
   
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_presence_of :permalink
+  validates_length_of :permalink, :within => 3..255
+  validates_uniqueness_of :permalink
+
   scope :sorted, lambda {order("pages.position ASC") }
   scope :newest_first, lambda {order("pages.created_at ASC") }
   
--- app/models/section.rb
+++ app/models/section.rb
@@ -4,6 +4,13 @@ class Section < ActiveRecord::Base
   has_many :section_edits
   has_many :editors, :through => :section_edits, :class_name => "AdminUser"
   
+  CONTENT_TYPES = ['text', 'HTML']
+  
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_inclusion_of :content_type, :in => CONTENT_TYPES, :message => "must be one of: #{CONTENT_TYPES.join(',')}}"
+  validates_presence_of :content
+
   scope :sorted, lambda {order("sections.position ASC") }
   scope :newest_first, lambda {order("sections.created_at ASC") }
   
