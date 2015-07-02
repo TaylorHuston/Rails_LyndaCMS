aee50d280eb5c06aef2a9b2bacc16d46de68d6b4
Taylor Huston <taylor.a.huston@gmail.com>
2015-07-01 23:42:52 -0700

    Change to postgres for development

--- db/migrate/20150520021315_add_password_digest_to_admin_users.rb
+++ db/migrate/20150520021315_add_password_digest_to_admin_users.rb
@@ -6,6 +6,6 @@ class AddPasswordDigestToAdminUsers < ActiveRecord::Migration
   
   def down
     remove_column "admin_users", "password_digest"
-    add_column "admin_users", "hashed_password", :string. :limit => 40
+    add_column "admin_users", "hashed_password", :string, :limit => 40
   end
 end
